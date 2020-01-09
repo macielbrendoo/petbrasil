c3f8ddf31595cf2de065108c0e723460a9b58da2
Brendo Maciel <maciel.brendoo@gmail.com>
2020-01-09 17:47:45 -0300

    Create Candidato Endpoint

---
+++ src/main/java/br/com/macielbrendoo/petbrasil/endpoint/CandidatoEndpoint.java
@@ -0,0 +1,47 @@
+package br.com.macielbrendoo.petbrasil.endpoint;
+
+import br.com.macielbrendoo.petbrasil.model.Candidato;
+import br.com.macielbrendoo.petbrasil.repository.CandidatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("candidato")
+public class CandidatoEndpoint {
+    private final CandidatoRepository candidatoDAO;
+
+    @Autowired
+    public CandidatoEndpoint(CandidatoRepository candidatoDAO) {
+        this.candidatoDAO = candidatoDAO;
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<?> getCandidatoByCpf(@PathVariable String cpf) {
+        if (candidatoDAO.findById(cpf).isPresent()) {
+            return new ResponseEntity<>(candidatoDAO.findById(cpf).get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Não encontrado candidato com o cpf informado",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<?> createCandidato(@RequestBody Candidato candidato) {
+        if(candidatoDAO.findById(candidato.getCpf()).isPresent()) {
+            return new ResponseEntity<>("Usuário já cadastrado", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(candidatoDAO.save(candidato),HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<?> deleteCandidato(@PathVariable String cpf) {
+        candidatoDAO.deleteById(cpf);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateCandidato(@RequestBody Candidato candidato) {
+        return new ResponseEntity<>(candidatoDAO.save(candidato), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/br/com/macielbrendoo/petbrasil/repository/CandidatoRepository.java
@@ -0,0 +1,8 @@
+package br.com.macielbrendoo.petbrasil.repository;
+
+import br.com.macielbrendoo.petbrasil.model.Candidato;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CandidatoRepository extends CrudRepository<Candidato, String> {
+
+}
