3c79dcc8c071683c42c35129f52f7f186c7d453e
Brendo Maciel <maciel.brendoo@gmail.com>
2020-01-10 17:50:20 -0300

    Create getter and setter for enderecoPK

--- src/main/java/br/com/macielbrendoo/petbrasil/model/Endereco.java
+++ src/main/java/br/com/macielbrendoo/petbrasil/model/Endereco.java
@@ -64,4 +64,31 @@ class enderecoPK implements Serializable{
     private int numero;
     private String cep;
     private String complemento;
+
+    public enderecoPK() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
